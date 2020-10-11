064969b11a0f79b7b12e12fc2926a286e6641521
Vicente Marçal <vicente.marcal@gmail.com>
2020-10-10 22:54:42 -0400

    feat(project) - Issue 3: Loggin to Telegram Account and list mega groups of the logged user

---
+++ src/teladduser/teladduser.py
@@ -0,0 +1,47 @@
+import click
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from decouple import config
+
+
+@click.command()
+def teladduser():
+    """Log in on a Telegram account and list all megagroup of it."""
+    # Login on a Telegram account
+    api_id = config('API_ID')
+    api_hash = config('API_HASH')
+    phone = config('PHONE')
+    client = TelegramClient(phone, api_id, api_hash)
+    client.connect()
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        client.sign_in(phone, input('Enter the Login Code that was send to yor Telegram app: '))
+    # Get all Groups of the logged user
+    chats = []
+    last_date = None
+    chunk_size = 100
+    groups = []
+    result = client(
+        GetDialogsRequest(
+            offset_date=last_date,
+            offset_id=0,
+            offset_peer=InputPeerEmpty(),
+            limit=chunk_size,
+            hash=0
+        )
+    )
+    # Get only the mega group of the logged user
+    chats.extend(result.chats)
+    for chat in chats:
+        try:
+            if chat.megagroup:
+                groups.append(chat)
+        except:
+            continue
+    # Select a group to add users
+    for i, g in enumerate(groups):
+        print(f"{i} - {g.title}")
+    g_index = input("Enter Number of Group you want add users: ")
+    target_group = groups[int(g_index)]
+    print(f'The selected group was: {target_group.title}')
